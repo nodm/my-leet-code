e034467ba274ecc0314365cd43210a2abe29b4c6
Dmytro Novikov <nodm.pe@gmail.com>
2024-04-28 07:55:42 +0300

    Merge Sorted Array

--- src/sorting-and-searching/1-easy/merge-sorted-array/index.test.ts
+++ src/sorting-and-searching/1-easy/merge-sorted-array/index.test.ts
@@ -1,19 +1,27 @@
-import {merge} from './index';
+import merge from './index';
 
 describe('Testing "merge"', () => {
   test.each([
     {
       num1: [1, 2, 3, 0, 0, 0],
-      n: 3,
-      num2: [2, 5, 6],
       m: 3,
+      num2: [2, 5, 6],
+      n: 3,
       expected: [1, 2, 2, 3, 5, 6],
     },
-    {num1: [0], n: 0, num2: [1], m: 1, expected: [1]},
+    {num1: [1], m: 1, num2: [], n: 0, expected: [1]},
+    {num1: [0], m: 0, num2: [1], n: 1, expected: [1]},
+    {
+      num1: [4, 5, 6, 7, 9, 0, 0, 0, 0, 0],
+      m: 5,
+      num2: [1, 2, 4, 4, 8],
+      n: 5,
+      expected: [1, 2, 4, 4, 4, 5, 6, 7, 8, 9],
+    },
   ])(
     'should return $expected for the $num1 of length $n and $num2 of length $m',
-    ({num1, n, num2, m, expected}) => {
-      merge(num1, n, num2, m);
+    ({num1, m, num2, n, expected}) => {
+      merge(num1, m, num2, n);
       expect(num1).toEqual(expected);
     }
   );
--- src/sorting-and-searching/1-easy/merge-sorted-array/index.ts
+++ src/sorting-and-searching/1-easy/merge-sorted-array/index.ts
@@ -1,9 +1,67 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
-export function merge(
+export function merge1(
   nums1: number[],
   m: number,
   nums2: number[],
   n: number
-): void {}
+): void {
+  if (n === 0) return;
+
+  for (let i = 0; i < m; i += 1) {
+    if (nums1[i] > nums2[0]) {
+      swap(nums1, i, nums2, 0);
+
+      /** reorder the second array */
+      let j = 1;
+      while (j < n && nums2[j] < nums2[j - 1]) {
+        swap(nums2, j, nums2, j - 1);
+        j += 1;
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i += 1) {
+    nums1[m + i] = nums2[i];
+  }
+}
+
+function swap(nums1: number[], m: number, nums2: number[], n: number) {
+  const temp = nums1[m];
+  nums1[m] = nums2[n];
+  nums2[n] = temp;
+}
+
+/**
+ * O(n + m)
+ */
+export function merge2(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
+  let i = m - 1; // last element's index in nums1
+  let j = n - 1; // last element's index in nums2
+  let k = m + n - 1; // last element's index of the merged array
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i -= 1;
+    } else {
+      nums1[k] = nums2[j];
+      j -= 1;
+    }
+    k -= 1;
+  }
+
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j -= 1;
+    k -= 1;
+  }
+}
+
+export default merge2;
