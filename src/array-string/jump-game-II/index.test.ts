c057755e9a95c286f77747bcba6812c49995ff1c
Dmytro Novikov <nodm.pe@gmail.com>
2024-06-18 18:12:33 +0300

    Jump Game II

---
+++ src/array-string/jump-game-II/index.test.ts
@@ -0,0 +1,15 @@
+import {jump} from './index';
+describe('jump', () => {
+  test.each([
+    [[2, 3, 1, 1, 4], 2],
+    [[2, 3, 0, 1, 4], 2],
+    [[0], 0],
+    [[1, 2, 3], 2],
+    [[1, 1, 1, 1], 3],
+  ])(
+    'should return the minimum number of jumps needed to reach the last index for the array %s',
+    (nums, expected) => {
+      expect(jump(nums)).toBe(expected);
+    }
+  );
+});
